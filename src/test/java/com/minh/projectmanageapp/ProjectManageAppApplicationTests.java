97d2c4eb3b8b622bb1775ebfbfa9df61dca9e2cd
Minh <tuanminh191919@gmail.com>
2023-03-07 14:22:13 +0700

    start

---
+++ src/test/java/com/minh/projectmanageapp/ProjectManageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minh.projectmanageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
